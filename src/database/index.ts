583823442f1d3f03beee94d98269232e5308e366
Roberto Barbosa <betoanselmo@gmail.com>
2023-10-02 19:07:20 -0300

    Dot env

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
 
 class DbConnection {
 	async connect() {
 		try {
-
+			await mongoose.connect(process.env.NODE_ENV);
 			console.log('connect to database');
 		} catch (error) {
 			console.log('Error to coneect database: ' + error);
